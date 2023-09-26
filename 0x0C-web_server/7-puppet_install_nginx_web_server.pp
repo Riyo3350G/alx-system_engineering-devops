a03b423cdcd821f2d1e4a81e5d8e4e8c8a87e946
Redwan Ben Yechou <67595559+Riyo3350G@users.noreply.github.com>
2023-09-26 21:49:31 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,34 +1,41 @@
-# Configure Nginx to listen on port 80
-class nginx::config {
-  package { 'nginx': ensure => present }
-
-  service { 'nginx':
-    ensure => running,
-    enabled => true,
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure => present,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    content => template('nginx/default.conf'),
-  }
+# Install The Nginx package
+package { 'nginx':
+  ensure => installed,
 }
 
-# Create a simple HTML file
+# Ensure Nginx service is enabled & running
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Configure Nginx to listen on port 80 and set the default page HTML
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "server {
+    listen 80 default_server;
+    root /var/www/html;
+    index index.html;
+    location / {
+        echo 'Hello World!';
+    }
+    location /redirect_me {
+        return 301 http://www.example.com;
+    }
+  }",
+  notify  => Service['nginx'],
+  require => Package['nginx'],
+}
+
+# Create the document root directory
+file { '/var/www/html':
+  ensure => directory,
+}
+
+# Create a simple index.html file
 file { '/var/www/html/index.html':
-  ensure => present,
+  ensure  => file,
   content => 'Hello World!',
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-}
-
-# Redirect /redirect_me to /
-nginx::vhost { 'redirect_me':
-  ensure => present,
-  server_name => 'localhost',
-  location => '/redirect_me',
-  rewrite => { '^/$ permanent', },
+  require => File['/var/www/html'],
 }
