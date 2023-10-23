00e719ece7f2a667648d53bc11a6da459f6d1b40
riyo3350g <yechouben@gamail.com>
2023-10-23 20:16:23 +0100

    Task 1 added

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""""
+Script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODOs list progress.
+"""
+import csv
+from requests import get
+import sys
+
+
+def user_data_from_api(userId):
+    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    name = user.json().get('name')
+    response = get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                   .format(userId))
+    data = response.json()
+
+    with open("{}.csv".format(userId), mode="w") as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for task in data:
+            ls = [userId, name, task.get('completed'), task.get('title')]
+            csv_writer.writerow(ls)
+
+
+if __name__ == "__main__":
+    user_data_from_api(sys.argv[1])
