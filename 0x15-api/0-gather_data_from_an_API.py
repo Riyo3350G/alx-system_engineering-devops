b6170d34ef706ef2ba4a1af143a4b85c6e4e2131
riyo3350g <yechouben@gamail.com>
2023-10-23 20:03:30 +0100

    Task 0 update comment

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """"
 Script that, using this REST API,
 for a given employee ID, returns information
-about his/her TODO list progress.
+about his/her TODOs list progress.
 """
 from requests import get
 import sys
