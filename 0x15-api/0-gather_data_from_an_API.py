6f9fe1e9940e17c2d8be66a75942b1fff4725711
riyo3350g <yechouben@gamail.com>
2023-10-23 21:02:40 +0100

    Task 0 update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,58 +1,31 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
+""""
+Script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODOs list progress.
+"""
+from requests import get
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    # Define the API endpoint
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos'
+def user_data_from_api(userId):
+    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    name = user.json().get('name')
+    response = get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                   .format(userId))
+    data = response.json()
 
-    # Fetch user data
-    user_response = requests.get(user_url)
-
-    if user_response.status_code != 200:
-        print(f"Error: Could not retrieve user data for ID {employee_id}")
-        return
-
-    user_data = user_response.json()
-    user_name = user_data.get('name')
-
-    if user_name is None:
-        print(f"Error: User name not found for ID {employee_id}")
-        return
-
-    # Fetch todos data for a specific user using the query parameter
-    params = {"userId": employee_id}
-    todos_response = requests.get(todos_url, params=params)
-
-    if todos_response.status_code != 200:
-        print(f"Error: Could not retrieve data for user ID {employee_id}")
-        return
-
-    todos_data = todos_response.json()
-
-    if not todos_data:
-        print(f"No TODO list data found for user ID {employee_id}")
-        return
-
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo.get('completed'))
-
-    print(f"Employee {user_name} is done with tasks"
-          f"({completed_tasks}/{total_tasks}):")
-
-    for todo in todos_data:
-        if todo.get('completed'):
-            print(f"     {todo.get('title')}")
+    total_task = len(data)
+    completed_task = 0
+    for task in data:
+        if task.get('completed') is True:
+            completed_task += 1
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed_task, total_task))
+    for task in data:
+        if task.get('completed') is True:
+            print("\t {}".format(task.get('title')))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    user_data_from_api(sys.argv[1])
