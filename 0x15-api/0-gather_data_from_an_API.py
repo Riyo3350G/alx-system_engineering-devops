24575ee10fcde2314f3ab85af7ce6626ae922dfe
riyo3350g <yechouben@gmail.com>
2023-10-23 13:50:58 -0700

    Task 0 try

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,22 +9,22 @@ import sys
 
 
 def user_data_from_api(userId):
-    user = get('https://jsonplaceholder.typicode.com/users/{}'.format(userId))
+    user = get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
     name = user.json().get("name")
-    response = get('https://jsonplaceholder.typicode.com/users/{}/todos'
+    response = get("https://jsonplaceholder.typicode.com/users/{}/todos"
                    .format(userId))
     data = response.json()
 
     total_task = len(data)
     completed_task = 0
     for task in data:
-        if task.get('completed') is True:
+        if task.get("completed") is True:
             completed_task += 1
-    print('Employee {} is done with tasks({}/{}):'
+    print("Employee {} is done with tasks({}/{}):"
           .format(name, completed_task, total_task))
     for task in data:
-        if task.get('completed') is True:
-            print("\t {}".format(task.get('title')))
+        if task.get("completed") is True:
+            print("\t {}".format(task.get("title")))
 
 
 if __name__ == "__main__":
