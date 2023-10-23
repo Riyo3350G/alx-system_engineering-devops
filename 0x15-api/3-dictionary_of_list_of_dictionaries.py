e4a581eef4175321623123d76238388c0dd3b7e1
riyo3350g <yechouben@gamail.com>
2023-10-23 20:44:48 +0100

    Task 3 added

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""""
+Script that, using this REST API,
+for a given employee ID, returns information
+about his/her TODOs list progress.
+"""
+import json
+from requests import get
+import sys
+
+
+def user_data_from_api():
+
+    with open("todo_all_employees.json", mode="w") as f:
+        for i in range(1, 11):
+            user = get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(i))
+            username = user.json().get('username')
+            resp = get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                       .format(i))
+            data = resp.json()
+            ls = []
+            task_data = {}
+            for task in data:
+                row = {}
+                row["task"] = task.get('title')
+                row["completed"] = task.get('completed')
+                row["username"] = username
+                ls.append(row)
+            task_data[i] = ls
+            json.dump(task_data, f)
+
+
+if __name__ == "__main__":
+    user_data_from_api()
